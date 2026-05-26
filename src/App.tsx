9317d42669c0b2029a7e40a7e635380a9d0c5eb5
biajikit <biajikit@gmail.com>
2026-05-26 17:48:40 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
+// 自动获取代理前缀 /sandboxID/port
+const getBasename = () => {
+  const parts = window.location.pathname.split("/").filter(Boolean);
+  if (parts.length >= 2) {
+    return `/${parts[0]}/${parts[1]}`;
+  }
+  return "";
+};
+
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* 后续加页面，直接在这里写 Route 就行 */}
-        </Routes>
-      </Router>
+    <Router basename={getBasename()}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* 后续加页面，直接在这里写 Route 就行 */}
+      </Routes>
+    </Router>
   )
 }
 

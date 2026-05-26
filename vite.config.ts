99cc4b87639a34657708cd1b41d583638fabe873
biajikit <biajikit@gmail.com>
2026-05-26 17:03:13 +0800

    Update vite.config.ts
    测试代理

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  // 解决 HMR / 路由 / 路径问题
+  base: './',
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 443,
+      path: '/'
+    }
+  }
 })

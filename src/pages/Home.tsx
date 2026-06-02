4177c973b914e27e896697818aa0409cb1bcaf98
biajikit@123 <biajikit@gmail.com>
2026-06-02 14:57:37 +0800

    doing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,22 @@ import ShopCard from '@/components/GoodsCard/index'
 import CategoryNav from '@/components/CategoryNav/index'
 import ShopDetail from '@/components/PublicFooter/index'
 import {useScrollTrigger} from "@/hooks/useTriggerVisibility.tsx";
+import {getStorage} from "@/core/publicFn.ts";
+import {languageKeyList} from "@/assets/dict/pageLanguage.ts";
+import {useLanguage} from "@/assets/dict/language.tsx";
 
 
 export default function Home() {
     let {scrollContainerRef} = useScrollTrigger()
+    // 根据用户浏览器修改页面默认语言
+    if (!getStorage('lang')) {
+        let locale = 'EN'
+        languageKeyList.forEach((language) => {
+            if (language.key.toLowerCase() === navigator.language.toLowerCase()) locale = language.key
+        })
+        const {setLang} = useLanguage() // 语言设置
+        setLang(locale)
+    }
     return (
         <div>
             <PublicHeader></PublicHeader>
